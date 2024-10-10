1335a6dc66762d6664bbcdb5f5a9f9fbbca8dd90
sizophila10 <61324667+sizophila10@users.noreply.github.com>
2024-10-10 21:12:49 +0200

    added api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: "I was a little girl when I traveled here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+  let context =
+    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. ";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt:${prompt}`);
+  console.log(`Context:${context}`);
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");

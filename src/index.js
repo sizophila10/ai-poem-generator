f6e24eb2f8f48b89d3e8c6b90bb6aba63040384d
sizophila10 <61324667+sizophila10@users.noreply.github.com>
2024-10-10 21:54:24 +0200

    improved the user experience

--- src/index.js
+++ src/index.js
@@ -19,6 +19,10 @@ function generatePoem(event) {
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating the poem about ${instructionsInput.value} </div>`;
+
   console.log("Generating poem");
   console.log(`Prompt:${prompt}`);
   console.log(`Context:${context}`);
